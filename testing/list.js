5ea71bcae84769eb1b7067fe482857ed3a04b0ee
princii16 <59653692+princii16@users.noreply.github.com>
2020-03-09 21:40:07 +0530

    testing

---
+++ testing/list.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDJ5N734amKIPxuIqBfvW9c1_Y4OQ9gwjQ",
+    authDomain: "tiarareg-9672a.firebaseapp.com",
+    databaseURL: "https://tiarareg-9672a.firebaseio.com",
+    projectId: "tiarareg-9672a",
+    storageBucket: "tiarareg-9672a.appspot.com",
+    messagingSenderId: "617156753642",
+    appId: "1:617156753642:web:3ff00b1ef1ee170b677f93",
+    measurementId: "G-F91FECFKPK"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
