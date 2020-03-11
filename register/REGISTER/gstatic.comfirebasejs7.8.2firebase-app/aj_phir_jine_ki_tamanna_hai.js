7dd2b1ea663f173596d7e4f7f9f9d619913d17f8
satyam <sazalpandeyj2@gmail.com>
2020-03-11 21:20:28 +0530

    Register update

---
+++ register/REGISTER/gstatic.comfirebasejs7.8.2firebase-app/aj_phir_jine_ki_tamanna_hai.js
@@ -0,0 +1,126 @@
+
+/*var collegeId = "";
+var collegeDocId = "";
+var collegeName = "";
+var collegeCode = "";
+var collegeAmount = "";
+
+loadIt();
+*/
+function loadIt(){
+    document.getElementById("loading-spinner").style.display = "block";
+    document.getElementById("main-content").style.display = "none";
+}
+
+function showIt(){
+    document.getElementById("loading-spinner").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
+}
+
+
+
+
+const db = firebase.firestore();
+ 
+
+$(document).keydown(function(e){
+  if(e.which === 123){
+     return false;
+  }
+});
+
+  function getStudent(){
+    //loadIt();
+      var table = "<table id='example'><thead><tr><th>SID</th><th>Name</th><th>College</th><th>Phone</th><th>E - Mail</th><th>Attendance</th></tr></thead><tbody id='table'>";
+      var count = 0 ; 
+      db.collection("Attendance")
+      .get().then((snapshot)=>{
+          snapshot.docs.forEach(doc=>{
+              count++;
+            table += "<tr><td>"+ doc.data()["sid"] + "</td><td>"+ doc.data()["name"] + "</td><td>" + doc.data()["college_name"] + "</td><td>" + doc.data()["contact"] + "</td><td>"+ doc.data()["email"] + "</td><td>"+ doc.data()["attendance"] + "</td></tr>";
+          })
+          table += "</tbody></table>";
+          document.getElementById("tableHere").innerHTML = table;
+          collegeAmount = count *200 ; 
+         
+      });
+  }
+
+  function myStudent(){
+    loadIt();
+    var get_sid = document.getElementById("search").value.trim();
+
+    var docRef = db.collection("student");
+
+      docRef.where("sid" ,"==",get_sid).get().then(function(snapShot) {
+        var i =0;
+        var s="";var n="";var c="";var p="";var e="";
+        snapShot.docs.forEach(doc=>{
+          
+          s =  doc.data()["sid"];
+          n =  doc.data()["name"];
+          c =  doc.data()["college_name"];
+          p =  doc.data()["contact"];
+          e =  doc.data()["email"];
+          i++;
+        });
+          if (i==1) {
+            // var a =  doc.data()["attendance"];
+   
+             document.getElementById("sid_student").innerHTML = s;
+             document.getElementById("name_student").innerHTML = n;
+             document.getElementById("college_student").innerHTML = c;
+             document.getElementById("phone_student").innerHTML = p;
+             document.getElementById("email_student").innerHTML = e;
+           //  document.getElementById("attendance_student").innerHTML = a;
+          showIt();
+
+          } else {
+              
+              alert("No Record Found. Check SID again");
+              showIt();
+          }
+      }).catch(function(error) {
+          console.log("Error getting document:", error);showIt();
+      });
+  }
+  
+  function addStudent(){
+    loadIt();
+      var sid_student = document.getElementById("sid_student").innerHTML;
+      var name_student = document.getElementById("name_student").innerHTML;
+      var college_student = document.getElementById("college_student").innerHTML;
+      var phone_student = document.getElementById("phone_student").innerHTML;
+      var email_student = document.getElementById("email_student").innerHTML;
+      var attendance_student = document.getElementById("attendance_student").innerHTML;
+    
+      var count = 0;
+
+      db.collection("Attendance")
+      .get().then((snapshot)=>{
+          snapshot.docs.forEach(doc=>{
+              count++;
+          });
+
+          if(count == 0){
+              db.collection("Attendance")
+              .add({
+                  sid : sid_student,
+                  name : name_student,
+                  college_name : college_student,
+                  email : email_student,
+                  contact : phone_student,
+                  attendance : attendance_student
+              }).then((snapshot="0")=>{
+                showIt();
+              })
+             
+          }else{
+           // resetTheForm();
+              window.alert("Student has already received the ID Card.");
+              showIt();
+          }
+      });
+
+  }
+
